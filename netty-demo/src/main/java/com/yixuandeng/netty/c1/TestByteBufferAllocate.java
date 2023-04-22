3d4f91cc76e982f42aa0680ff715a9921cff921e
Yi Xuan Deng <850671214@qq.com>
2023-04-22 16:38:00 +0800

    Yixuan Deng Branch

--- netty-demo/src/main/java/com/yixuandeng/netty/c1/TestByteBufferAllocate.java
+++ netty-demo/src/main/java/com/yixuandeng/netty/c1/TestByteBufferAllocate.java
@@ -12,5 +12,14 @@ import java.nio.ByteBuffer;
  * @date 22/04/2023 15:55
  */
 public class TestByteBufferAllocate {
+    public static void main(String[] args) {
+        System.out.println(ByteBuffer.allocate(16).getClass());
+        System.out.println(ByteBuffer.allocateDirect(16).getClass());
+        /**
+         * class java.nio.HeapByteBuffer - java 堆内存 读写效率低 收到GC的影响
+         * class java.nio.DirectByteBuffer - java 直接内存 读写效率高 少一次数据的拷贝 直接使用系统的内存 不受gc影响
+         */
+
+    }
 
 }
---
+++ netty-demo/src/main/java/com/yixuandeng/netty/c1/TestByteBufferString.java
@@ -0,0 +1,17 @@
+package com.yixuandeng.netty.c1;/**
+ * @Author 85067
+ * @create 22/04/2023 16:37
+ */
+
+/**
+ * @author 85067
+ * @version 1.0
+ * @description: TODO
+ * @date 22/04/2023 16:37
+ */
+public class TestByteBufferString {
+    public static void main(String[] args) {
+
+    }
+
+}
