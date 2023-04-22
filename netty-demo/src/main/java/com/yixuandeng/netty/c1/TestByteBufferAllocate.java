ef64624e33665fc421a3e371a5f040dfbdeb9e62
Yi Xuan Deng <850671214@qq.com>
2023-04-22 15:55:28 +0800

    commit

---
+++ netty-demo/src/main/java/com/yixuandeng/netty/c1/TestByteBufferAllocate.java
@@ -0,0 +1,13 @@
+package com.yixuandeng.netty.c1;/**
+ * @Author 85067
+ * @create 22/04/2023 15:55
+ */
+
+/**
+ * @author 85067
+ * @version 1.0
+ * @description: TODO
+ * @date 22/04/2023 15:55
+ */
+public class TestByteBufferAllocate {
+}
